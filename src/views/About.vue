f0503ccfb85b4f1037a94e7a3e16788052782691
memory <2410732744@qq.com>
2019-03-29 23:34:12 +0800

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div>
+  <el-container>
+    <el-header class="brandHeader" height="45px">
+     库存品牌
+    </el-header>
+    <el-main>
+      主要内容
+    </el-main>
+    <el-footer>
+      底部
+    </el-footer>
+  </el-container>
+</div>
 </template>
+
+<script>
+export default {
+  name: 'about',
+  components: {
+  }
+}
+</script>
+<style scoped lang="less">
+  @mainColor: rgba(45, 42, 183, 0.78);
+  .el-header{
+    background: @mainColor;
+    text-align: center;
+    line-height: 45px;
+    color: black;
+  }
+</style>
+
+
+
